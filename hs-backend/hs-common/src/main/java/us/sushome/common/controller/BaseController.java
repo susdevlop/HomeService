f92de4e820213a1bd3379a1cbfb8b6602606091a
TzuHsing <12410733+tzuhsing@user.noreply.gitee.com>
2024-07-18 11:40:15 +0800

    配置工具类

---
+++ hs-backend/hs-common/src/main/java/us/sushome/common/controller/BaseController.java
@@ -0,0 +1,29 @@
+package us.sushome.common.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import us.sushome.common.utils.DateUtils;
+
+import java.beans.PropertyEditorSupport;
+import java.util.Date;
+
+public class BaseController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    /**
+     * 将前台传递过来的日期格式的字符串，自动转化为Date类型
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        // Date 类型转换
+        binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                setValue(DateUtils.parseDate(text));
+            }
+        });
+    }
+}
--- hs-backend/hs-web/src/main/java/us/sushome/hsweb/controller/HsUserController.java
+++ hs-backend/hs-web/src/main/java/us/sushome/hsweb/controller/HsUserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import us.sushome.common.controller.BaseController;
 import us.sushome.db.model.HsUser;
 import us.sushome.db.service.IHsUserService;
 
@@ -12,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class HsUserController {
+public class HsUserController extends BaseController {
     @Autowired
     private IHsUserService iHsUserService;
 
     @PostMapping("/getAllUser")
     public List<HsUser> getAllUser(){
-        return iHsUserService.getAllUser();
+        List<HsUser> list = iHsUserService.getAllUser();
+        logger.info("list"+list);
+        return list;
     }
 }
