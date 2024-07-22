5ba93e9acdedb0305f9ea4722ac64807f9334f17
tzuhsingsu <ai6900144@gmail.com>
2024-07-23 00:47:43 +0800

    更新数据库

--- hs-backend/init.sql
+++ hs-backend/init.sql
@@ -5,15 +5,37 @@ CREATE SCHEMA IF NOT EXISTS home_service COLLATE utf8mb4_general_ci;
 USE home_service;
 
 -- 创建表，如果不存在则创建
+
 CREATE TABLE IF NOT EXISTS hs_user
 (
-    user_id        VARCHAR(60)       NOT NULL COMMENT 'id',
-    user_name      VARCHAR(50)       NOT NULL,
-    user_passwd    VARCHAR(60)       NOT NULL,
-    user_sex       TINYINT DEFAULT 0 NOT NULL COMMENT '1 male 2 female',
-    user_email     VARCHAR(50)       NULL COMMENT 'email',
-    user_cellphone VARCHAR(15)       NULL,
-    user_openid    VARCHAR(128)      NULL,
-    PRIMARY KEY (user_id)
-    )
-    COMMENT 'user table';
+    user_id        varchar(60)         not null comment 'id'
+        primary key,
+    user_name      varchar(50)         not null,
+    user_passwd    varchar(60)         not null,
+    user_sex       tinyint  default 0  not null comment '1 male 2 female',
+    user_email     varchar(50)         null comment 'email',
+    user_cellphone varchar(15)         null,
+    user_openid    varchar(128)        null,
+    user_role_id   smallint default -1 not null comment '对应的权限 id'
+)
+    comment 'user table';
+
+
+
+create table home_service.hs_permission
+(
+    pm_id         int auto_increment
+        primary key,
+    pm_role_id    smallint    not null,
+    pm_permission varchar(50) not null
+)
+    comment '权限表';
+
+create table home_service.hs_roles
+(
+    roles_id     smallint          not null comment '权限 id',
+    roles_name   varchar(10)       not null comment '权限名',
+    roles_enable tinyint default 1 not null comment '0关闭 1开启'
+)
+    comment '权限表';
+
